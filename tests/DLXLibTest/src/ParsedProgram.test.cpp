3748fee26ecae93a5ff42daec3d230842ac9b417
AMS21 <19614997+AMS21@users.noreply.github.com>
2021-11-06 11:39:45 +0100

    [Test] Add test for `dlx::ParsedProgram`

---
+++ tests/DLXLibTest/src/ParsedProgram.test.cpp
@@ -0,0 +1,32 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include <DLX/InstructionLibrary.hpp>
+#include <DLX/ParsedProgram.hpp>
+#include <DLX/Parser.hpp>
+
+TEST_CASE("ParsedProgram::GetDump()")
+{
+    // Empty
+    dlx::ParsedProgram prog;
+    std::string        str;
+
+    str = prog.GetDump();
+    CHECK(!str.empty());
+
+    // With error
+    dlx::ParseError err;
+    err.column      = 1;
+    err.line_number = 1;
+    err.message     = "Test";
+    prog.m_ParseErrors.emplace_back(err);
+
+    str = prog.GetDump();
+    CHECK(!str.empty());
+
+    // Normal program
+    dlx::InstructionLibrary lib;
+    prog = dlx::Parser::Parse(lib, "l: ADD R1 R1 R1\nJ l");
+
+    str = prog.GetDump();
+    CHECK(!str.empty());
+}
