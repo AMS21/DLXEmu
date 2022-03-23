a8c1ac495b2d3a1e2c24e6616a3d8264633af3e1
AMS21 <AMS21.github@gmail.com>
2022-03-23 13:51:13 +0100

    [DLXEmu] Mark io as const

--- DLXEmu/src/Window.cpp
+++ DLXEmu/src/Window.cpp
@@ -163,7 +163,7 @@ namespace dlxemu
         glClear(GL_COLOR_BUFFER_BIT);
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
-        ImGuiIO& io = ImGui::GetIO();
+        const ImGuiIO& io = ImGui::GetIO();
 
         // Update and Render additional Platform Windows
         // (Platform functions may change the current OpenGL context, so we save/restore it to make it easier to paste this code elsewhere.
