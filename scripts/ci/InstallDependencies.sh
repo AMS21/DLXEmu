8652ad890c556a2d94b8795b86c0008f406e2490
AMS21 <AMS21.github@gmail.com>
2022-12-11 19:36:53 +0100

    [scripts] Install jinja2 via apt instead of pip

--- scripts/ci/InstallDependencies.sh
+++ scripts/ci/InstallDependencies.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 apt-get update -m
-apt-get install -y cmake libglu1-mesa-dev libxrandr-dev ninja-build pip python xorg-dev
-
-pip install jinja2
+apt-get install -y cmake libglu1-mesa-dev libxrandr-dev ninja-build pip python python-jinja2 xorg-dev
