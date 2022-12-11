244b2a30632fcf51c473b02dca23c38e731209a0
AMS21 <AMS21.github@gmail.com>
2022-12-11 19:09:49 +0100

    [scripts] Actually add all required dependencies to `InstallDependencies.sh`

--- scripts/ci/InstallDependencies.sh
+++ scripts/ci/InstallDependencies.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-sudo apt-get update -m
-sudo apt-get install xorg-dev libglu1-mesa-dev libxrandr-dev
+apt-get update -m
+apt-get install -y cmake libglu1-mesa-dev libxrandr-dev ninja-build pip python xorg-dev
+
+pip install jinja2
