57f06d11e7057eabdc3d87865156290b9a06abb1
AMS21 <AMS21.github@gmail.com>
2022-12-12 05:55:49 +0100

    [scripts] Fix build problems with python2/3

--- scripts/ci/InstallDependencies.sh
+++ scripts/ci/InstallDependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 apt-get update -m
-apt-get install -y cmake libglu1-mesa-dev libxrandr-dev ninja-build pip python python-jinja2 xorg-dev
+apt-get install -y cmake libglu1-mesa-dev libxrandr-dev ninja-build pip python3 python3-jinja2 xorg-dev
