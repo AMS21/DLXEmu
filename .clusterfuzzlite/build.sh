895b15e1861d1be50ebd6921f8a598e97c287488
AMS21 <AMS21.github@gmail.com>
2022-12-13 08:54:02 +0100

    [clusterfuzzlite] Remove helper targets since they can't be fuzzed

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -6,12 +6,15 @@ cd build
 
 # Configure cmake
 cmake .. -G Ninja \
-    -DCMAKE_C_COMPILER="${CC}" -DCMAKE_CXX_COMPILER="${CXX}" \
-    -DCMAKE_C_FLAGS="${CFLAGS} -DPHI_CONFIG_ENABLE_ASSERTIONS" -DCMAKE_CXX_FLAGS="${CXXFLAGS} -DPHI_CONFIG_ENABLE_ASSERTIONS" \
-    -DPHI_FUZZING_LIBRARY:STRING="${LIB_FUZZING_ENGINE}"
+	-DCMAKE_C_COMPILER="${CC}" -DCMAKE_CXX_COMPILER="${CXX}" \
+	-DCMAKE_C_FLAGS="${CFLAGS} -DPHI_CONFIG_ENABLE_ASSERTIONS" -DCMAKE_CXX_FLAGS="${CXXFLAGS} -DPHI_CONFIG_ENABLE_ASSERTIONS" \
+	-DPHI_FUZZING_LIBRARY:STRING="${LIB_FUZZING_ENGINE}"
 
 # Build fuzzers
 ninja tests/Fuzzing/all
+
+# Remove helper targets
+rm -f bin/generate_dictionary_app bin/convert_structured_text
 
 # Copy fuzzers to out
 cp bin/* "${OUT}/"
