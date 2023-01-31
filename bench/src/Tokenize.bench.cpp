25e244001038764dc6c518e399ed269f748a64ea
AMS21 <AMS21.github@gmail.com>
2023-01-31 09:07:23 +0100

    [Benchmarks] Add a bit of `constexpr`

--- bench/src/Tokenize.bench.cpp
+++ bench/src/Tokenize.bench.cpp
@@ -39,8 +39,8 @@ BENCHMARK(BM_TokzenizeRandom)->RangeMultiplier(2)->Range(8, 8 << 17)->Complexity
 
 static void BM_TokzenizeADD(benchmark::State& state)
 {
-    std::int64_t count         = state.range(0);
-    std::int64_t string_length = 4;
+    std::int64_t                        count         = state.range(0);
+    static constexpr const std::int64_t string_length = 4;
 
     // Prepare string
     std::string string;
